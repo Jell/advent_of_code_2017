109acbba91359fae1feac2d07f54f5c02f415ec5
Jell <jean-louis@jawaninja.com>
2017-12-04 20:39:51 +0100

    Strict PureScript Day 3 part 1

---
+++ purs/src/Day3.purs
@@ -0,0 +1,66 @@
+module Day3 where
+
+import Data.Boolean
+import Data.Maybe
+import Prelude
+
+import Data.Array as Array
+import Data.Foldable (sum, and)
+import Data.Generic (class Generic, gShow)
+import Data.Lazy (Lazy, defer, force)
+import Data.List.Lazy (List, Step(..), take, (:), nil, last, iterate, index)
+import Data.Ord (abs)
+import Data.Traversable as T
+import Data.Tuple (Tuple(..), fst)
+import Partial.Unsafe (unsafePartial)
+
+data Dir = Right | Up | Left | Down
+derive instance genericDir :: Generic Dir
+
+instance showDir :: Show Dir where
+  show = gShow
+
+target :: Int
+target = 361527
+
+nextDir :: Dir -> Dir
+nextDir Right = Up
+nextDir Up    = Left
+nextDir Left  = Down
+nextDir Down  = Right
+
+data Coords = Coords Int Int
+derive instance genericCoords :: Generic Coords
+
+instance showCoords :: Show Coords where
+  show = gShow
+
+transition :: Dir -> Coords -> Boolean
+transition Right (Coords x y) = x == -y
+transition Up    (Coords x y) = x == (y + 1)
+transition Left  (Coords x y) = (x - 1) == -y
+transition Down  (Coords x y) = x == (y - 1)
+
+coordsStep :: Dir -> Coords -> Coords
+coordsStep Right (Coords x y) = Coords (x + 1) y
+coordsStep Up    (Coords x y) = Coords x (y + 1)
+coordsStep Left  (Coords x y) = Coords (x - 1) y
+coordsStep Down  (Coords x y) = Coords x (y - 1)
+
+coordsIter :: Int -> (Tuple Coords Dir) -> (Tuple Coords Dir)
+coordsIter 0 r = r
+coordsIter n (Tuple c dir) = coordsIter (n - 1) (Tuple newCoords newDir)
+  where newDir = if (transition dir c)
+                 then (nextDir dir)
+                 else dir
+        newCoords = (coordsStep dir c)
+
+
+coords :: Int -> Coords
+coords n = fst $ coordsIter n (Tuple (Coords 0 0) Right)
+
+dist :: Coords -> Int
+dist (Coords x y) = (abs x) + (abs y)
+
+part1 :: Int
+part1 = dist $ coords target
--- purs/src/Main.purs
+++ purs/src/Main.purs
@@ -3,6 +3,7 @@ module Main where
 import Prelude
 import Day1 as Day1
 import Day2 as Day2
+import Day3 as Day3
 import Partial.Unsafe (unsafePartial)
 
 import Control.Monad.Eff (Eff)
@@ -20,3 +21,6 @@ main = do
 
   log "Day 2 - Part 2"
   log $ show Day2.part2
+
+  log "Day 3 - Part 1"
+  log $ show Day3.part1
