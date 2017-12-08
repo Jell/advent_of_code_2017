caa5cf199c5bd8a2bac1d601a7e542bb4116c5d5
Jell <jean-louis@jawaninja.com>
2017-12-08 12:33:45 +0100

    Day 8 clj

---
+++ clj/day8.clj
@@ -0,0 +1,42 @@
+(require '[clojure.string :as str])
+
+(def input
+  (-> (slurp "day8_input.txt")
+      (str/split #"\n")))
+
+(defn parse [line]
+  (let [[target op-str op-val-str _ cond-target cond-op-str cond-val-str]
+        (str/split line #" ")
+
+        op-val (read-string op-val-str)
+
+        op (case op-str
+             "inc" (fnil (partial + op-val) 0)
+             "dec" (fnil (partial + (- op-val)) 0))
+
+        cond-op (case cond-op-str
+                  ">" >
+                  "<" <
+                  ">=" >=
+                  "<=" <=
+                  "==" =
+                  "!=" not=)
+
+        cond-val (read-string cond-val-str)]
+    (fn [state]
+      (update state target
+              (fn [val] (if (cond-op (get state cond-target 0) cond-val)
+                          (op val)
+                          (or val 0)))))))
+
+;; Part 1
+(->> (map parse input)
+     (reduce (fn [state step] (step state)) {})
+     vals
+     (apply max))
+
+;; Part 2
+(->> (map parse input)
+     (reductions (fn [state step] (step state)) {})
+     (mapcat vals)
+     (apply max))
